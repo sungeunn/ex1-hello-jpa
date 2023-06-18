164448dc90edb141151356a1ed08fba4fa54013a
sungeunn <saeun0512@gmail.com>
2023-06-18 23:56:59 +0900

    feat: 영속성 컨텍스트 2

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,15 +16,11 @@ public class JpaMain {
 
         try {
 
-            //비영속
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("HelloJPA");
-
             //영속
-            System.out.println("=== BEFORE ===");
-            em.persist(member);
-            System.out.println("=== AFTER ===");
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZZ");
+
+            System.out.println("===========================");
 
             tx.commit();
         } catch (Exception e) {
